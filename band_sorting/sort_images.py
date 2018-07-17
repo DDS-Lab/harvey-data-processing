99a93948ecad424312e4164eccbc87a342aa4fdf
tessajean <31619981+tessajean@users.noreply.github.com>
2018-07-18 01:10:52 +0200

    Update sort_images.py

--- band_sorting/sort_images.py
+++ band_sorting/sort_images.py
@@ -3,20 +3,23 @@ import os
 import shutil
 
 BAND_NUM = 3
+POST_EVENT_DIR = "/Volumes/My Book/Harvey/postevent/images"
+POST_EVENT_SORT3BAND = '/Volumes/My Book/Harvey/postevent/sorted_images/3_band/'
+POST_EVENT_SORT1BAND = '/Volumes/My Book/Harvey/postevent/sorted_images/1_band/'
 
-for date in os.listdir("/Volumes/My Book/Harvey/postevent/images"):
+for date in os.listdir(POST_EVENT_DIR):
     print (date)
-    os.chdir("/Volumes/My Book/Harvey/postevent/images/"+date)
-    for folder in os.listdir("/Volumes/My Book/Harvey/postevent/images/"+date):
-        os.chdir("/Volumes/My Book/Harvey/postevent/images/"+date+"/"+folder)
-        for img in os.listdir("/Volumes/My Book/Harvey/postevent/images/"+date+"/"+folder):
+    os.chdir(POST_EVENT_DIR+date)
+    for folder in os.listdir(POST_EVENT_DIR+date):
+        os.chdir(POST_EVENT_DIR+date+"/"+folder)
+        for img in os.listdir(POST_EVENT_DIR+date+"/"+folder):
             if img.split('.')[-1] == 'tif':
                 image = gdal.Open(img)
                 number_of_bands = image.RasterCount
                 if number_of_bands == BAND_NUM:
-                    new_path = ('/Volumes/My Book/Harvey/postevent/sorted_images/3_band/'+date+'_'+folder+'_'+img)
+                    new_path = (POST_EVENT_SORT3BAND+date+'_'+folder+'_'+img)
                     shutil.copy(img, new_path)
                     print ('copied ' + img + ' to ' + new_path)
                 else:
-                    new_path = ('/Volumes/My Book/Harvey/postevent/sorted_images/1_band/'+date+'_'+folder+'_'+img)
+                    new_path = (OST_EVENT_SORT1BAND+date+'_'+folder+'_'+img)
                     shutil.copy(img, new_path)
