6d76ad47cac945d94dc2b862cb7f49a275f33ea0
Tessa Schneider <tessajeanschneider@gmail.com>
2018-07-18 11:29:01 +0200

    fix referencing script.py

--- downloading/script.py
+++ downloading/script.py
@@ -3,16 +3,16 @@ created by Daniel Cao
 to create bash script to download all the catalog id from the links
 download them to their respective catalog folders
 """
-POST_EVENT_CAT = 'postevent_catalogs.txt'
+POST_EVENT_CAT = 'txt_files/postevent_catalogs.txt'
 
 catalogs = open(POST_EVENT_CAT, 'r').readlines()
 
 for catalog in catalogs:
     print(catalog)
     catalog2 = catalog.strip()
-    # Open txt file of the iterated catalog ID
-    link_list = open(catalog2 + '.txt', 'r').readlines()
-    script = open(catalog2 + '.sh', 'w+')
+    # Open txt file of the iterated catalog ID 
+    link_list = open('txt_files/' + catalog2 + '.txt', 'r').readlines()
+    script = open('txt_files/' + catalog2 + '.sh', 'w+')
 
     # Writes the bash script that makes a directory for a catalog ID, enters it, downloads from the links, and exits dir
     script.write('#!/bin/bash\n')
