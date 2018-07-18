ffc494e47280d03fee5178e5e2eb6e4dfdd7a64e
tessajean <31619981+tessajean@users.noreply.github.com>
2018-07-18 19:19:31 +0200

    Update parse.py

--- downloading/parse.py
+++ downloading/parse.py
@@ -16,6 +16,10 @@ page = "http://www.digitalglobe.com/opendata/hurricane-harvey/post-event"
 req = requests.get(page)
 soup = BeautifulSoup(req.content, "html.parser")
 
+#create directory to save txt files to
+os.makedir('/txt_files/')
+os.chdir('/txt_files/')
+
 # get download links per Catalog ID
 for textarea in soup.findAll("textarea"):	
     links_file = open(str(textarea['id']).split('-')[1] + '.txt', 'w+')
